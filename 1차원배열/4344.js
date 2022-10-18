92ad56b51c0a90a8306a92921c5fe51c3f8b96ce
hae9 <gorud0929@naver.com>
2022-10-18 22:31:43 +0900

    221018

--- 1차원배열/4344.js
+++ 1차원배열/4344.js
@@ -1,20 +1,17 @@
 const input = require('fs').readFileSync('input.txt').toString().split('\n');
 // console.log(input[0]);
-
+// console.log(input);
 for (let i = 1; i <= input[0]; i += 1) {
-  let arr = input[i]
-    .trim()
-    .split(' ')
-    .map((ele) => parseInt(ele));
-  console.log(arr);
+  let arr = input[i].split(' ');
+  // console.log(arr);
   let sum = 0;
   for (let j = 1; j <= arr[0]; j += 1) {
-    sum += arr[j];
+    sum += Number(arr[j]);
   }
-  console.log(sum);
-  let average = sum / arr[0];
-  console.log(average);
+  // console.log(sum);
+  let average = Number(sum / arr[0]);
+  // console.log(average);
   let res = arr.filter((elem) => elem > average);
-  console.log(res);
+  // console.log(res);
   console.log(`${((res.length / arr[0]) * 100).toFixed(3)}%`);
 }
