23eaf3f3ced7e1db5e47a7641e4d4d20bfb5fd1f
hae9 <gorud0929@naver.com>
2023-01-12 13:06:42 +0900

    230112 greedy Q1

--- greedy/1946.js
+++ greedy/1946.js
@@ -7,9 +7,34 @@ const input = require('fs')
   .trim()
   .split('\n');
 
-console.log(input);
-let i;
-for (i = 0; i < input.length; i++) {
-  i += Number(input[i]);
-  console.log(i);
+let array = [];
+
+for (let i = 2; i < input.length; i++) {
+  const applicant = input[i].trim().split(' ').map(Number);
+  if (applicant.length === 1) {
+    sol(array);
+    array = [];
+  } else {
+    array.push(applicant);
+  }
+
+  if (i === input.length - 1) {
+    sol(array);
+  }
+}
+
+function sol(array) {
+  const newArr = array;
+  newArr.sort((a, b) => a[0] - b[0]);
+
+  let min = newArr[0][1];
+  let answer = 1;
+  for (let i = 1; i < newArr.length; i++) {
+    const num = newArr[i][1];
+    if (min > newArr[i][1]) {
+      min = newArr[i][1];
+      answer++;
+    }
+  }
+  console.log(answer);
 }
